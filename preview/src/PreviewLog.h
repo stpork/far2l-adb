feb79488e5bf4006b3f77535bf083b26c4dfca3a
stpork <stpork@gmail.com>
2026-05-24 23:34:29 +0300

    preview: gate DBG on DEBUG/_DEBUG; template no-op for release

--- preview/src/PreviewLog.h
+++ preview/src/PreviewLog.h
@@ -1,22 +1,35 @@
 #ifndef PREVIEWLOG_H
 #define PREVIEWLOG_H
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifndef NDEBUG
-void PreviewDebugLog(const char* format, ...);
-#define DBG(fmt, ...) PreviewDebugLog("[%s] " fmt, __FUNCTION__, ##__VA_ARGS__)
-#else
-#define DBG(fmt, ...) ((void)0)
+#if defined(DEBUG) || defined(_DEBUG)
+void PreviewDebugLog(const char *format, ...);
 #endif
 
 #ifdef __cplusplus
 }
+#endif
+
+// Debug macros - completely absent from release builds; enable with -DDEBUG.
+// Release path uses a variadic template no-op so DBG arguments are still
+// parsed and counted as "used" (no -Wunused / no [[maybe_unused]] needed);
+// the empty body is DCE'd to nothing.
+#if defined(DEBUG) || defined(_DEBUG)
+#define DBG(fmt, ...) PreviewDebugLog("[%s] " fmt, __FUNCTION__, ##__VA_ARGS__)
+#else
+#ifdef __cplusplus
+template<typename... Args>
+inline void PreviewDbgNoOp(Args &&...) {}
+#define DBG(fmt, ...) PreviewDbgNoOp(fmt, ##__VA_ARGS__)
+#else
+#define DBG(fmt, ...) ((void)0)
+#endif
 #endif
 
 #endif // PREVIEWLOG_H
